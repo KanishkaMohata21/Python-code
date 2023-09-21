3f2773d04e0083558fc64d8553af479336146eed
KanishkaMohata21 <132136054+KanishkaMohata21@users.noreply.github.com>
2023-09-22 00:17:54 +0530

    Add files via upload

---
+++ life_in_weeks.py
@@ -0,0 +1,6 @@
+age=input(" Enter your age: ")
+years_left=90-int(age)
+months_left=years_left*12
+weeks_left=years_left*52
+days_left=years_left*365
+print(f" No of years left is {years_left}\n No of months left is {months_left}\n No of weeks left is {weeks_left}\n No of days left is {days_left} ")
