26294fa3d0f4d002b70478ae7e3db2b007e3cf61
KanishkaMohata21 <132136054+KanishkaMohata21@users.noreply.github.com>
2023-09-23 12:07:18 +0530

    Add files via upload

---
+++ rock_paper_scissor.py
@@ -0,0 +1,29 @@
+import random
+user_choice=int(input("What is your choice?\nEnter 0 for rock,1 for scissor,2 for paper:"))
+if user_choice>=3 or user_choice<0:
+    print("Invalid number")
+else:
+    if user_choice==0:
+            print("You choose rock")
+    elif user_choice==1:
+            print("You choose paper")
+    else:
+            print("You choose scissor")
+    computers_choice=random.randint(0,2)
+    if computers_choice==0:
+        print("Computer choose rock")
+    elif computers_choice==1:
+        print("Computer choose paper")
+    else:
+        print("Computer choose scissor")
+
+    if user_choice==0 and computers_choice==2:
+        print("You won")
+    elif computers_choice>user_choice:
+        print("You lose")
+    elif computers_choice==0 and user_choice==2:
+        print("You lose")
+    elif user_choice>computers_choice:
+        print("You win")
+    else:
+        print("Draw")
