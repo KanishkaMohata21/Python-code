70fa0413f689dd1e483aef8f769bebfac6feb532
KanishkaMohata21 <132136054+KanishkaMohata21@users.noreply.github.com>
2023-12-28 20:17:28 +0530

    Add files via upload

---
+++ workout_tracking.py
@@ -0,0 +1,51 @@
+import requests
+from datetime import datetime
+
+API_ID=""
+API_KEY=""
+SHEETY_ENDPOINT=""
+
+GENDER = "Female"
+WEIGHT_KG = 70
+HEIGHT_CM = 150 
+AGE = 19
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": API_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
+
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(SHEETY_ENDPOINT, json=sheet_inputs)
+
+    print(sheet_response.text)
